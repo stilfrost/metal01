863911819b523cc6ecfd60a7d9f6705fa6e55a53
stillfrost <stillfrost@163.com>
2022-04-24 22:07:12 +0800

    的点点滴滴多多

---
+++ src/com/sy/metal02.java
@@ -0,0 +1,14 @@
+package com.sy;
+
+/**
+ * @author: stillfrost
+ * @ClassName: metal02
+ * @date: 2022/4/24 21:58
+ * @Description
+ * @since JDK 1.8
+ **/
+public class metal02 {
+    public static void main(String[] args) {
+        System.out.println("的点点滴滴多多多");
+    }
+}
