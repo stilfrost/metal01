8c9815257f01761f08c0816cadabfee2ce781c0c
stillfrost <stillfrost@163.com>
2022-04-24 21:54:42 +0800

    简历假那开荒离开家欧派

---
+++ src/com/sy/shuangyan.java
@@ -0,0 +1,14 @@
+package com.sy;
+
+/**
+ * @author: stillfrost
+ * @ClassName: shuangyan
+ * @date: 2022/4/24 21:51
+ * @Description
+ * @since JDK 1.8
+ **/
+public class shuangyan {
+    public static void main(String[] args) {
+        System.out.println("双言");
+    }
+}
